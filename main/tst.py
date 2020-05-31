ce541c6453c1a9ceca9f0d67f032f762e47a3b8b
Labitoku <louis.euvrardarnaud@gmail.com>
2020-05-31 15:24:32 +0200

    Getting line
    Adding how to get a line + test files

--- main/tst.py
+++ main/tst.py
@@ -1,1 +1,17 @@
-print ("cc")
+from odf import text, teletype
+from odf.opendocument import load
+
+"""#charger et afficher un texte
+testdoc = load("test/a_textTest.odt")
+print(testdoc.text)
+"""
+
+"""#detecter les differentes lignes
+"""
+testdoc = load("test/b_DetectionLigne.odt")
+allparas = testdoc.getElementsByType(text.P)
+#teletype.extractText(allparas[0])
+
+for i in range(len(allparas)):
+    print(str(i) + "          " + str(allparas[i]))
+
