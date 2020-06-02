ea1663cab281bfce011eb5be687403c44eaeb091
Labitoku <louis.euvrardarnaud@gmail.com>
2020-06-03 00:21:44 +0200

    Adding noteset local storage
    The truc can now stock all the notes, depending on the line they are located on the original partition

--- main/main.py
+++ main/main.py
@@ -7,22 +7,22 @@ class Gamme:
     
     def __init__(self):
         self.notes = ["C", "D", "E", "F", "G", "A", "B"]
-        self.notesDown = ["C", "Db", "D", "Eb", "E", "Fb", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-        self.notesUp = ["C", "D", "D#", "E", "E#", "F", "F#", "G", "G#", "A", "A#", "B", "B#"]        
+        self.notesBemol = ["C", "Db", "D", "Eb", "E", "Fb", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+        self.notesDiese = ["C", "D", "D#", "E", "E#", "F", "F#", "G", "G#", "A", "A#", "B", "B#"]        
         self.bemol = "b"
         self.diese = "#"
 
 
     def switchNote(self, note, upping, qte):
         if upping:
-            for i in range(len(self.notesUp)):
-                if self.notesUp[i] == note.note:
-                    note.note = self.notesUp[(i + qte) % len(self.notesUp)]            
+            for i in range(len(self.notesDiese)):
+                if self.notesDiese[i] == note.note:
+                    note.note = self.notesDiese[(i + qte) % len(self.notesDiese)]            
                     break
         else:
-            for i in range(len(self.notesDown)):
-                if self.notesDown[i] == note.note:
-                    note.note = self.notesDown[(i + qte) % len(self.notesDown)]            
+            for i in range(len(self.notesBemol)):
+                if self.notesBemol[i] == note.note:
+                    note.note = self.notesBemol[(i + qte) % len(self.notesBemol)]            
                     break
 
         return note
@@ -30,8 +30,29 @@ class Gamme:
 
 class Note:
 
-    def __init__(self, valeur):
-        self.note = valeur
+    def __init__(self, valeur, tuning = ""):
+        self.note = valeur + tuning
+
+class NoteSet:
+
+    def __init__(self, ligne):
+        self.notes = []
+        self.gamme = Gamme()
+        text = teletype.extractText(ligne)
+        
+        for i in range(len(text)):
+            if text[i] in self.gamme.notes:
+                tuning = text[i+1]
+                self.notes.append(Note(text[i], tuning if tuning == "b" or "#" else ""))
+
+    def show_noteset(self):
+        noteset = "["
+        for i in range(len(self.notes)):
+            noteset += self.notes[i].note
+            if i != len(self.notes):
+                noteset += ", "
+        noteset += "]"
+        print(noteset)
 
 
 
@@ -39,40 +60,69 @@ class Partition:
 
     def __init__(self, opendocument):
         self.gamme = Gamme()
-        self.partition = opendocument
-        self.lignes = []
-        self.line_identification()
-
-
-    def line_identification(self):
-        allText = self.partition.getElementsByType(text.P)
-        #teletype.extractText(allText[0])
-        for i in range(len(allText)):
-            #if(allText[i])
-            if self.noteline_identification(allText[i]):
-                print("Ligne de note à la ligne : " + str(i))
-
-            
-
-    def noteline_identification(self, ligne):
-        text = teletype.extractText(ligne)
         
+        self.partition = opendocument
+        self.allText = self.partition.getElementsByType(text.P)
+        
+        self.bemol = False
+        self.diese = False
+        
+        self.lignes = []
+        self.notes = []
+        self.cpt = 0
+        
+        self.line_identification()
+        self.set_notes()
+
+    #permet de repérer quelles lignes sont pour les notes desquelles sont pour les paroles
+    def line_identification(self):
+        #teletype.extractText(allText[0])
+        for i in range(len(self.allText)):
+            if self.noteline_identification(self.allText[i]):
+                self.lignes.append(i)
+                #print("Ligne de note à la ligne : " + str(self.lignes[self.cpt]))
+                self.cpt += 1
+
+    #permet de détecter une ligne de notes  -> determine aussi si on est en diese ou en bemol
+    def noteline_identification(self, ligne):
+        text = teletype.extractText(ligne)       
         for i in range(len(text)):
-            if(len(text) > 5):
-                if(self.check_char_note(text[i])):
-                    if(text[i+1].lower() == 'm' or text[i+1] == ' ' or text[i+1] == '#' or text[i+1] == 'b'):
-                        return True
-
-#                    if(text[i+1] == ' '):
- #                       if(text[i+2] == ' '):
-   #                         if(text[i+3] == ' '):
-    #                            return True
-
+            if(self.check_char_note(text[i])):
+                if(text[i+1].lower() == "m" or text[i+1] == " " or text[i+1] == "#" or text[i+1] == "b"):
+                    if(text[i+1] == "b"):
+                        self.bemol = True
+                    elif(text[i+1] == "#"):
+                        self.diese = True
+                    if(text[i+1] == "/" or text[i+2] == "/"):
+                        print("note vener")
+                    return True
         return False
 
     def check_char_note(self, char):
-        if(char in gamme.notes):
+        if(char in self.gamme.notes):
             return True
+
+    def set_notes(self):
+        for i in range(len(self.lignes)):
+            self.notes.append(NoteSet(self.allText[self.lignes[i]]))
+            self.notes[i].show_noteset()
+
+    #permet d'obtenir un index d'une note dans le tableau correspondant
+    def get_char_index(self, char, bemol):
+        if(bemol):
+            for i in self.gamme.notesBemol:
+                if gamme.notesBemol[i] == char:
+                    return i
+        else:
+            for i in self.gamme.notesDiese:
+                if gamme.notesDiese[i] == char:
+                    return i
+
+    def switch_type(self):
+        if(self.bemol):
+            for i in range(len(self.lignes)):
+                j = 4         
+
 
     """def check_line_word(self, line):
         if()
